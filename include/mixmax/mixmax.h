4555cd03a18987d1b6c2c0a16ce9f2c62c221588
DiamonDinoia <m.barbone19@imperial.ac.uk>
2023-10-13 16:12:46 -0700

    added copyright

--- include/mixmax/mixmax.h
+++ include/mixmax/mixmax.h
@@ -1,6 +1,34 @@
-//
-// Created by mbarbone on 11/3/22.
-//
+/*
+ *  PLEASE NOTE - THIS IS AN IMPLEMENTATION OF THE MIXMAX GENERATOR
+ *  EXPLICITLY FOR THE PURPOSE OF GPU PERFORMANCE MEASUREMENTS TO ACCOMPANY
+ *  THE PAPER IN TBA PROCEEDINGS TBA
+ *  Talk: CHEP 2023 Fast, high-quality pseudo random number generators for heterogeneous computing
+ *  https://indico.jlab.org/event/459/contributions/11828/
+ *  
+ *  Copyright 2023 Marco Barbone
+ *
+ *  original implementation found in mixmax.hpp source: https://mixmax.hepforge.org/
+ *
+ *  Copyright (2008-2016) by Konstantin Savvidy.
+ *
+ *  Free to use, academic or commercial. Do not redistribute without permission.
+ *
+ *	G.K.Savvidy and N.G.Ter-Arutyunian,
+ *  On the Monte Carlo simulation of physical systems,
+ *	J.Comput.Phys. 97, 566 (1991);
+ *  Preprint EPI-865-16-86, Yerevan, Jan. 1986
+ *
+ *  K.Savvidy
+ *  The MIXMAX random number generator
+ *  Comp. Phys. Commun. 196 (2015), pp 161–165
+ *  http://dx.doi.org/10.1016/j.cpc.2015.06.003
+ *
+ *  K.Savvidy and G.Savvidy
+ *  Spectrum and Entropy of C-systems. MIXMAX random number generator
+ *  Chaos, Solitons & Fractals, Volume 91, (2016) pp. 33–38
+ *  http://dx.doi.org/10.1016/j.chaos.2016.05.003
+ *
+ */
 
 #ifndef MIXMAX_INCLUDE_MIXMAXRNG_H_
 #define MIXMAX_INCLUDE_MIXMAXRNG_H_
